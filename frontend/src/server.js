f11320063c7f99a23038125e7ce26d63198844c4
Shagun-katariya <126535072+Shagun-katariya@users.noreply.github.com>
2024-10-01 13:53:24 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://ecommerce-zeta-orcin-87.vercel.app/api/v2";
 export const backend_url = "http://localhost:8000/";
 
 
