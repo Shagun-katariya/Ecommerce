6704b773073abd288f4806534c022f4acd1e8108
Shagun-katariya <126535072+Shagun-katariya@users.noreply.github.com>
2024-11-19 22:09:46 +0530

    Update Sponsored.jsx

--- frontend/src/components/Route/Sponsored.jsx
+++ frontend/src/components/Route/Sponsored.jsx
@@ -23,14 +23,14 @@ const Sponsored = () => {
         </div>
         <div className="flex items-start">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/LG_logo_%282015%29.svg/2560px-LG_logo_%282015%29.svg.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/976px-Apple_logo_black.svg.png?20220821121934"
             style={{width:"150px", objectFit:"contain"}}
             alt=""
           />
         </div>
         <div className="flex items-start">
           <img
-            src="https://www.vectorlogo.zone/logos/apple/apple-ar21.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg"
             style={{width:"150px", objectFit:"contain"}}
             alt=""
           />
