fa01d0e6c2309d074ada0a3440fce692f0f39656
Shagun-katariya <126535072+Shagun-katariya@users.noreply.github.com>
2024-11-19 21:16:53 +0530

    Update shop.js

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -39,7 +39,7 @@ router.post("/create-shop", catchAsyncErrors(async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 
-    const activationUrl = `https://ecommerce-frontend-olive-iota.vercel.app/seller/activation/${activationToken}`;
+    const activationUrl = `https://ecommerce-frontend-rndk3ci5n.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
